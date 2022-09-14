6ba583b9a4de46b48753ed86a34bf6926618c19b
BePine <bepine17@gmail.com>
2022-09-14 23:54:42 +0200

    Added scripts folder, changed loader script loc, added redirection with loading, created second page, header and footer

---
+++ Scripts/Loader.js
@@ -0,0 +1,26 @@
+const heading = document.querySelector('h1')
+const loadingHover = document.querySelector('.loadingHover')
+    let count = 0
+    let secCount = 0
+    const onloadScript = () => {
+        setInterval(changeHeading, 700);
+      
+    }
+    const changeHeading = () => {
+        if(count<3){
+            heading.innerHTML += '.'
+            count++
+        }
+        else if(secCount===1){
+            secCount=0
+            loadingHover.style.display="flex"
+            setTimeout(() => {
+                window.location.replace('sidePage.html')
+            }, 600);
+        }
+        else{
+            count=0
+            secCount++
+            heading.innerHTML = 'Website is work in progress'
+        }    
+    }
